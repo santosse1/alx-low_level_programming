de3368f0790b1d3d0cad2b155c24868185ca541b
ABDERRAZAQ IDLHAJ <abdo.ialhaj@gmail.com>
2023-03-17 10:45:52 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - A program that prints line with puts function
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+puts('=' \"Programming is like building a multilingual puzzle");
+return (0);
+}
+---
+
+5
+#include <stdio.h>
+/**
+ * main - A program that print a line using the printf function
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n")
+                return (0);
+}
+
