0c31228f0af6007eb87da7f10c7fb62136608502
ABDERRAZAQ IDLHAJ <abdo.ialhaj@gmail.com>
2023-03-23 21:45:39 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks if numbers are 0 - 9
+ * @c: int to be checked
+ *
+ * Return: 1 or 1
+ */
+
+int _isdigit(int c)
+{
+if (c >= '0' && c <= '9')
+return (1);
+else
+return (0);
+}
+
