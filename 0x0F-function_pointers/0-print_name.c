12a3192b47d7aa329d574b8f51f8c5851c404686
ABDERRAZAQ IDLHAJ <abdo.ialhaj@gmail.com>
2023-04-19 16:53:34 -0700

    Updated

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,16 +1,16 @@
 #include "function_pointers.h"
 #include <stdio.h>
 /**
- * print_IDLHAJ - print IDLHAJ using pointer to function
+ * print_name - print name using pointer to function
  * @name: string to add
  * @f: pointer to function
  * Return: nothing
  **/
-void print_IDLHAJ(char *name, void (*f)(char *))
+void print_name(char *name, void (*f)(char *))
 {
-	if (IDLHAJ == NULL || f == NULL)
+	if (name == NULL || f == NULL)
 		return;
 
-	f(IDLAHJ);
+	f(name);
 }
 
